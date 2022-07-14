e8454aa38eb43cfd4c57ae20569447559f34e3ba
ShubhamG1234 <100414848+ShubhamG1234@users.noreply.github.com>
2022-07-15 01:51:55 +0530

    Add files via upload
    creating prime number program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeNumberProgram
+{
+   class Number
+    {
+        public static void Main(string[] args)
+        {
+            int n = 50, b = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    b++;
+                }
+            }
+            if (b == 2) { Console.WriteLine("Number is Prime"); }
+            else { Console.WriteLine("Number is not Prime"); }
+        }
+    }
+}
